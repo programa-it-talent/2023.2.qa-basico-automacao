b9f5ff649b1e2272d7e5839559d90bb2ba8b0d9a
arthurmgui <arthurmottaguimaraes@outlook.com>
2023-10-20 12:16:01 -0300

    Primeira atividade

---
+++ src/test/java/arthur/PrimeiraAtividade.java
@@ -0,0 +1,13 @@
+package arthur;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PrimeiraAtividade
+{
+    @Test
+    public void primeiroTeste() {
+        int result = 1+1;
+        Assertions.assertEquals(2, result);
+    }
+}
