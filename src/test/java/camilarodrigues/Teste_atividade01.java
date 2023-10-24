5a979cfc87d2365dbb90fd96be8640fdb32acae9
Camila Rodrigues <rosac61@gmail.com>
2023-10-23 22:31:13 -0300

    Atividade 01

--- src/test/java/camilarodrigues/Teste_atividade01.java
+++ src/test/java/camilarodrigues/Teste_atividade01.java
@@ -1,2 +1,15 @@
-package camilarodrigues;public class Teste_atividade01 {
+package camilarodrigues;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Teste_atividade01
+{
+    @Test
+    @DisplayName("Cenario: 1 + 1 = 2")
+    public void teste01() {
+        int r = 1 + 1;
+        Assertions.assertEquals(2, r);
+    }
 }
