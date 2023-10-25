ee1d7c1bd1d13e03733947888dce54c638a2cabe
diegoaquino91 <di.aquino91@gmail.com>
2023-10-24 22:43:52 -0300

    segundo commit

---
+++ src/test/java/diegoaquino/Aula02Selenium.java
@@ -0,0 +1,23 @@
+package diegoaquino;
+
+import mickhill.Hooks;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class Aula02Selenium extends Hooks
+{
+    @Test
+    @DisplayName("Login válido")
+    public void teste02() throws InterruptedException
+    {
+        driver.get("https://the-internet.herokuapp.com/login");
+        By pathInputUsername = By.id("username");
+        driver.findElement(pathInputUsername).sendKeys("tomsmith");
+
+        By pathInputPassword = By.id("password");
+        driver.findElement(pathInputPassword).sendKeys("SuperSecretPassword!");
+
+        Thread.sleep(3000);
+    }
+}
