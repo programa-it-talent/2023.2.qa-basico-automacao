65b5fbde0651930f681ff46773c42dba5f138459
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2023-10-26 21:05:09 -0300

    Mapeando inoput pesquisa com CssSelector e Xpath

--- src/test/java/mickhill/Aula03MapeandoObjeto.java
+++ src/test/java/mickhill/Aula03MapeandoObjeto.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.By;
 
 public class Aula03MapeandoObjeto extends Hooks
 {
-    By pathInputPesquisa = By.id("APjFqb");
+    //By pathInputPesquisa = By.id("APjFqb");
+    //By pathInputPesquisa = By.cssSelector("textarea#APjFqb.gLFyf");
+    By pathInputPesquisa = By.xpath("//textarea[@id='APjFqb']"); // Caminho relativo
 
     @Test
     @DisplayName("Teste do Google")
@@ -14,6 +16,6 @@ public class Aula03MapeandoObjeto extends Hooks
     {
         driver.get("https://google.com.br");
         driver.findElement(pathInputPesquisa).sendKeys("Mick Lindo!");
-        Thread.sleep(2000);
+        Thread.sleep(5000);
     }
 }
