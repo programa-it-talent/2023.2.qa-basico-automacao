0aa1f487b63c8e74c26eda0d61ff27326aae86bf
AndreL0M <marquesandre.qa@gmail.com>
2023-10-29 20:10:00 -0300

    Primewiro teste it talent

---
+++ src/test/java/andremarques/Atividade_01.java
@@ -0,0 +1,16 @@
+package andremarques;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Atividade_01 {
+
+    @Test
+    public void teste_soma(){
+
+        int x = 3 * 3;
+        Assertions.assertEquals(9, x);
+
+    }
+
+}
